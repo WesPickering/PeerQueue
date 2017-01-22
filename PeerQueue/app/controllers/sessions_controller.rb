605d34cd9e31638e1bd1dcae0145e99810b7c54f
Joshua Zeitsoff <jzeitsoff@berkeley.edu>
2017-01-21 16:41:51 -0800

    Authentication working, put login/logout same page

--- PeerQueue/app/controllers/sessions_controller.rb
+++ PeerQueue/app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to '/test'
+      redirect_to '/'
     else
       redirect_to '/login'
     end
--- PeerQueue/app/controllers/users_controller.rb
+++ PeerQueue/app/controllers/users_controller.rb
@@ -17,4 +17,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :role)
   end
+
 end
