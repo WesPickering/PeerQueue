bdbc8793a7cc09ca22409e6672a12dfc27750dbe
Wesley Pickering <wesley.pickering@berkeley.edu>
2017-01-21 16:51:44 -0800

    working on buttons to functions

--- PeerQueue/app/controllers/users_controller.rb
+++ PeerQueue/app/controllers/users_controller.rb
@@ -7,11 +7,16 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to '/login'
     else
       redirect_to '/signup'
     end
   end
+
+  def help
+    render html:"kill me"
+  end
+
 
   private
   def user_params
--- PeerQueue/config/routes.rb
+++ PeerQueue/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :queued_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'signup' => 'users#new'
   resources :users
   get'login' => 'sessions#new'
   post'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+  get 'help_queued_user' => 'users#help'
 end
