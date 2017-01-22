4172cd3a191702ba1fe0f3b3bab7d1f6ee1a6fa7
Joshua Zeitsoff <jzeitsoff@berkeley.edu>
2017-01-22 00:48:52 -0800

    Added error message for same email

--- PeerQueue/app/controllers/users_controller.rb
+++ PeerQueue/app/controllers/users_controller.rb
@@ -1,14 +1,20 @@
 class UsersController < ApplicationController
+
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if User.find_by_email(user_params[:email])
+      flash[:notice] = "There is already an acount for this email. Please Login to create your board."
+      redirect_to '/'
+    elsif @user.save
       session[:user_id] = @user.id
       redirect_to '/queued_users'
     else
+      flash[:notice] = "One or more fields are empty."
       redirect_to '/'
     end
   end
