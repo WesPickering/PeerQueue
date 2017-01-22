1ceda797dd9eb61e2b9f7fd3f41e15917361826b
Wesley Pickering <wesley.pickering@berkeley.edu>
2017-01-22 00:28:56 -0800

    Heroku

--- PeerQueue/app/controllers/users_controller.rb
+++ PeerQueue/app/controllers/users_controller.rb
@@ -5,7 +5,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if User.find_by_email(user_params[:email])
+      redirect_to '/'
+    elsif @user.save
       session[:user_id] = @user.id
       redirect_to '/queued_users'
     else
