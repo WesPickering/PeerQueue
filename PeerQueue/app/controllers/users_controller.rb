081488310159f800bf5b56384a226d3135c58ac7
Joshua Zeitsoff <jzeitsoff@berkeley.edu>
2017-01-21 21:33:42 -0800

    Changed login pash, intrgrated bootstrap template

--- PeerQueue/app/controllers/users_controller.rb
+++ PeerQueue/app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/login'
+      redirect_to '/queued_users'
     else
       redirect_to '/signup'
     end
