3240ed8486bebdf69679b738d683b0f2b832666a
Joshua Zeitsoff <jzeitsoff@berkeley.edu>
2017-01-21 16:57:34 -0800

    Added downcasing

--- PeerQueue/app/models/user.rb
+++ PeerQueue/app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   has_secure_password
 
   def student?
-    self.role == 'student'
+    self.role.downcase! == 'student'
   end
   def ta?
-    self.role == 'ta'
+    self.role! == 'ta'
   end
   def instructor?
-    self.role == 'instructor'
+    self.role! == 'instructor'
   end
 end
